c9f20df2c57536f4b384f4fe33be37d8d7af5d08
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-02-06 23:11:18 +0200

    Add JSON helper and refactor specs

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,29 +2,25 @@
 
 describe ArticlesController do
   describe '#index' do
+    let!(:articles) { create_list(:article, 3) }
+    subject { get :index }
+
     it 'should return success response' do
-      get :index
+      subject
+
       expect(response).to have_http_status(:ok)
     end
-  end
 
-  it 'should return proper json' do
-    create_list(:article, 2)
+    it 'should return proper json' do
+      subject
 
-    get :index
-    json = JSON.parse(response.body)
-    # pp json
-    json_data = json['data']
-    expect(json_data.length).to eq(2)
-    expect(json_data[0]['attributes']).to eq({
-                                               'title' => 'My awesome article 1',
-                                               'content' => 'The content of my awesome article 1',
-                                               'slug' => 'my-awesome-article-1'
-                                             })
-    expect(json_data[1]['attributes']).to eq({
-                                               'title' => 'My awesome article 2',
-                                               'content' => 'The content of my awesome article 2',
-                                               'slug' => 'my-awesome-article-2'
-                                             })
+      articles.each_with_index do |article, index|
+        expect(json_data[index]['attributes']).to eq({
+                                                       'title' => article.title,
+                                                       'content' => article.content,
+                                                       'slug' => article.slug
+                                                     })
+      end
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -18,4 +20,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
+  config.include JsonApiHelpers
 end
