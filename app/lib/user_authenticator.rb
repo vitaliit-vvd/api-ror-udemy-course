e5dbe8fe505f867417ba4d0dc58ded1ef7830ed7
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-02-13 00:54:49 +0200

    Add authentication service: success

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -5,9 +5,27 @@ class UserAuthenticator
 
   attr_reader :user
 
-  def initialize(code); end
+  def initialize(code)
+    @code = code
+  end
 
   def perform
-    raise AuthenticationError
+    client = Octokit::Client.new(
+      client_id: Rails.application.credentials.dig(:github, :github_key),
+      client_secret: Rails.application.credentials.dig(:github, :github_secret)
+    )
+    token = client.exchange_code_for_token(code)
+    raise AuthenticationError if token.try(:error).present?
+
+    user_client = Octokit::Client.new(
+      access_token: token
+    )
+    user_data = user_client.user.to_h
+                           .slice(:login, :avatar_url, :url, :name)
+    User.create(user_data.merge(provider: 'github'))
   end
+
+  private
+
+  attr_reader :code
 end
