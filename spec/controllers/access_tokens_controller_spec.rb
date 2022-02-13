44be585c2f976a9b827e30060ec77ad5f36981ea
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-02-13 11:23:52 +0200

    Refactoring error tests

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,28 +2,6 @@
 
 RSpec.describe AccessTokensController, type: :controller do
   describe 'POST #create' do
-    shared_examples_for 'unauthorized_requests' do
-      let(:authentication_error) do
-        {
-          'status' => '401',
-          'source' => { 'pointer' => '/code' },
-          'title' => 'Authentication code is invalid',
-          'detail' => 'You must provide valid code in order to exchange it for token.'
-        }
-      end
-
-      it 'should return 401 status code' do
-        subject
-        expect(response).to have_http_status(401)
-      end
-
-      it 'should return proper error body' do
-        subject
-
-        expect(json['errors']).to include(authentication_error)
-      end
-    end
-
     context 'when no code provided' do
       subject { post :create }
 
@@ -77,29 +55,16 @@ RSpec.describe AccessTokensController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    context 'when invalid request' do
-      let(:authorization_error) do
-        {
-          'status' => '403',
-          'source' => { 'pointer' => '/headers/authorization' },
-          'title' => 'Not authorized',
-          'detail' => 'You have no right to access this resource.'
-        }
-      end
+    subject { delete :destroy }
 
-      subject { delete :destroy }
+    context 'when no authorization header provided' do
+      it_behaves_like 'forbidden_requests'
+    end
 
-      it 'should return 403 status code' do
-        subject
+    context 'when invalid authorization header provided' do
+      before { request.headers['authorization'] = 'Invalid token' }
 
-        expect(response).to have_http_status(:forbidden)
-      end
-
-      it 'should return proper error json' do
-        subject
-
-        expect(json['errors']).to include(authorization_error)
-      end
+      it_behaves_like 'forbidden_requests'
     end
 
     context 'when valid request' do
