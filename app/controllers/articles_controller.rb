ba36d10459daadcd96def59dcbe094ee884be706
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-02-13 20:17:12 +0200

    Add Articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,16 @@ class ArticlesController < ApplicationController
            status: :unprocessable_entity
   end
 
+  def update
+    article = Article.find(params[:id])
+    article.update!(article_params)
+    render json: article, status: :ok
+  rescue StandardError
+    render json: article, adapter: :json_api,
+           serializer: ErrorSerializer,
+           status: :unprocessable_entity
+  end
+
   private
 
   def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :articles, only: %i[index show create]
+  resources :articles, only: %i[index show create update]
   post 'login', to: 'access_tokens#create'
   delete 'logout', to: 'access_tokens#destroy'
 end
