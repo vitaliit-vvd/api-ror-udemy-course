6935a81b01a83002ef4508fab73fb3a2e2e048be
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-02-13 20:23:40 +0200

    Add allow to update only owned articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,7 @@ class Article < ApplicationRecord
   validates :title, :content, :slug, presence: true
   validates :slug, uniqueness: true
 
+  belongs_to :user
+
   scope :recent, -> { order(created_at: :desc) }
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -5,5 +5,7 @@ FactoryBot.define do
     sequence(:title) { |n| "My awesome article #{n}" }
     sequence(:content) { |n| "The content of my awesome article #{n}" }
     sequence(:slug) { |n| "my-awesome-article-#{n}" }
+
+    user
   end
 end
