f72552b400265cac8f76891bf99477aacfe51bdc
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-02-13 19:14:57 +0200

    Add Articles#create handle validation errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,5 +12,20 @@ class ArticlesController < ApplicationController
 
   def show; end
 
-  def create; end
+  def create
+    article = Article.new(article_params)
+    if article.valid?
+      # we will figure that out
+    else
+      render json: article, adapter: :json_api,
+             serializer: ActiveModel::Serializer::ErrorSerializer,
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    ActionController::Parameters.new
+  end
 end
