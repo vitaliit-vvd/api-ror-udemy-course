e34f3d73fe7b0a28e920080e8bb6659a2ca3801a
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-02-13 10:15:00 +0200

    Add routing for creating a session

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
 
 Rails.application.routes.draw do
   resources :articles, only: %i[index show]
+  post 'login', to: 'access_tokens#create'
 end
---
+++ spec/routing/access_tokens_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+describe 'access tokens routes' do
+  it 'should route to access_tokens create action' do
+    expect(post('/login')).to route_to('access_tokens#create')
+  end
+end
