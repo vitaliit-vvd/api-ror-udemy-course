92cae959840e26e9384ad0a9f65544a65e1bc3f2
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-06-10 10:11:44 +0300

    Add failure tests for standard auth service

--- app/lib/user_authenticator/standard.rb
+++ app/lib/user_authenticator/standard.rb
@@ -3,6 +3,8 @@
 class UserAuthenticator::Standard < UserAuthenticator
   class AuthenticationError < StandardError; end
 
+  attr_reader :user, :access_token
+
   def initialize(login, password); end
 
   def perform
