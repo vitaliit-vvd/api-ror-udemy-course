c976dcee0521667fb45a3e853da2e5dddc61c1c1
Vitalii Turzhanskyi <vitalii.t@vevidi.com>
2022-06-11 21:41:07 +0300

    Add valid request to registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 class RegistrationsController < ApplicationController
+  skip_before_action :authorize!, only: :create
 
+  def create
+    user = User.new(registration_params.merge(provider: 'standard'))
+    user.save!
+    render json: user, status: :created
+  end
+
+  private
+
+  def registration_params
+    params.require(:data).require(:attributes).
+      permit(:login, :password) ||
+      ActionController::Parameters.new
+  end
 end
